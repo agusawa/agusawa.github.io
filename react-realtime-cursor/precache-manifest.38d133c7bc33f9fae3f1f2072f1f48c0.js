72074c322bd9b216b409c7a0bd20eb939a3370db
Agus Stiawan <agus.stiawan2211@gmail.com>
2019-12-12 17:22:04 +0700

    init react-realtime-cursor app

---
+++ react-realtime-cursor/precache-manifest.38d133c7bc33f9fae3f1f2072f1f48c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82efc59c25f8bfca91f9897387c780d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bdca64c2fdc6d279d1e",
+    "url": "/static/css/main.f3017b72.chunk.css"
+  },
+  {
+    "revision": "65e9d3e59b0b894624d8",
+    "url": "/static/js/2.9cd37b44.chunk.js"
+  },
+  {
+    "revision": "ce408f1f6343b66e725e44e8745e2a18",
+    "url": "/static/js/2.9cd37b44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bdca64c2fdc6d279d1e",
+    "url": "/static/js/main.6820f53a.chunk.js"
+  },
+  {
+    "revision": "f5652256bd29ffd6bd20",
+    "url": "/static/js/runtime-main.178d8e1c.js"
+  }
+]);
